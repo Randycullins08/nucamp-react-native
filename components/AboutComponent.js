e2eda2d27692d87a88bda098d5b26accd67005fb
Randy Cullins <randycullins2018@gmail.com>
2021-04-20 06:09:27 -0600

    Animatable

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -5,6 +5,7 @@ import { Text } from "react-native";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
 import Loading from "./LoadingComponent";
+import * as Animatable from "react-native-animatable";
 
 const mapStateToProps = (state) => {
   return {
@@ -57,22 +58,26 @@ class About extends Component {
 
     if (this.props.partners.errMess) {
       <ScrollView>
-        <Mission />
-        <Card title={"Community Partners"}>
-          <Text>{this.props.partners.errMess}</Text>
-        </Card>
+        <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+          <Mission />
+          <Card title={"Community Partners"}>
+            <Text>{this.props.partners.errMess}</Text>
+          </Card>
+        </Animatable.View>
       </ScrollView>;
     }
     return (
       <ScrollView>
-        <Mission />
-        <Card title={"Community Partners"}>
-          <FlatList
-            data={this.props.partners.partners}
-            keyExtractor={(item) => item.id.toString()}
-            renderItem={renderPartner}
-          />
-        </Card>
+        <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+          <Mission />
+          <Card title={"Community Partners"}>
+            <FlatList
+              data={this.props.partners.partners}
+              keyExtractor={(item) => item.id.toString()}
+              renderItem={renderPartner}
+            />
+          </Card>
+        </Animatable.View>
       </ScrollView>
     );
   }
